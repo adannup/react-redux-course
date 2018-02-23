83efeb171eee3740ea588b5d22071b9bd515877c
Ramon Adan <adannup@users.noreply.github.com>
2018-02-23 13:37:49 -0600

    Separate components and containers

--- src/js/containers/App.js
+++ src/js/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import User from './User';
-import Main from './Main';
+import User from '../components/User';
+import Main from '../components/Main';
 import './App.scss';
 
 class App extends Component {
--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import { Provider } from 'react-redux';
-import App from './components/App';
+import App from './containers/App';
 
 const mathInitialState = {
   result: 1,
