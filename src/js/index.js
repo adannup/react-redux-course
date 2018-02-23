d35dd505f10fa175899140b2f3994fa8b73c9d98
Ramon Adan <adannup@users.noreply.github.com>
2018-02-23 15:02:15 -0600

    Implement react-hot-loader

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './containers/App';
-import store from './store';
+import { AppContainer } from 'react-hot-loader';
+import ReduxApp from './ReduxApp';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'),
-);
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root'),
+  );
+};
+
+render(ReduxApp);
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./ReduxApp', () => render(ReduxApp));
+}
